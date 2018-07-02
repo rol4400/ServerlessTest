1191dd35a5011a6b805df02a854f59d9447a4c58
Ryan Olsen <rol4400@outlook.com>
2018-07-02 20:52:36 +1000

    Fixed test run errors

--- app/test/route/user.js
+++ app/test/route/user.js
@@ -8,7 +8,7 @@ if (!process.env.NODE_ENV) {
 }
 
 var request = require('supertest');
-var app = require('../../app');
+var app = require('../../api');
 var id, user;
 
 describe('User routes:', function () {
