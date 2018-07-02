00e6d848616436997324704262493e10cac38d59
Ryan Olsen <rol4400@outlook.com>
2018-07-02 21:04:19 +1000

    Changed config so that tests run through the in-memory database

--- app/database.js
+++ app/database.js
@@ -29,11 +29,7 @@ module.exports.development = {
 };
 
 module.exports.test = {
-    driver     : 'mysql',
-    host       : 'localhost',
-    port       : '3306',
-    username   : 'root',
-    password   : 'password',
+    driver     : 'memory',
     database   : 'serverlesstest',
     autoReconnect : true
 };
